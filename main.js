339a6b1132eb515a6272a01b0639b9852ad50cbf
Tittou10 <prepa.issat@gmail.com>
2023-09-29 17:59:43 +0100

    Re-adjust the whole game logic

--- main.js
+++ main.js
@@ -1,149 +1,185 @@
-const btn = document.getElementById("btnstart");
 
+const btn = document.getElementById("btnstart");
+const board =document.querySelector(".board");
+const mes=document.getElementById('mesg');
+
+
+// Module for Gameboard
 const Gameboard = (() => {
-  let gameboard = ["", "", "", "", "", "", "", "", ""];
-  const getborad = () => {
-    return  gameboard ;
+  const gameboard = ["", "","", "", "", "", "", "", ""];
+
+  function getborad() {
+    return gameboard;
   };
+
+  function adjustboard (i,v)  {
+    
+      gameboard[i] = v;
+    }
   const setborad = () => {
-      gameboard=["", "", "", "", "", "", "", "", ""];
+    for (const index in gameboard) {
+      gameboard[index] ="";
+    }
   };
-  const render = () => {
-     count =0;
-     Gameboard.setborad();
+  const display = () => {
+    
     let inner = "";
     for (let index = 0; index < 9; index++) {
-      inner += `<button class="sq" id="${index}"></button>`;
+      inner += `<div class="sq" id="${index}"></div>`;
     }
-    document.querySelector(".board").innerHTML = inner;
+    board.innerHTML = inner;
 
     const squares = document.querySelectorAll(".sq");
     squares.forEach((s) => {
-      s.addEventListener("click", Game.hclick);
+      s.addEventListener("click", Game.play);
     });
+  };
+
+  return {
+    getborad,
+    setborad,
+    adjustboard,
+    display,
+  };
+})();
+
+
+const GameDisplay = (() => {
+  p1 = makeplayer(document.getElementById("p1").value, "X");
+  p2 = makeplayer(document.getElementById("p2").value, "O");
+ 
+
+  let currentplayer=p1;
+ 
+ 
+return {
+  p1,
+  p2,
+  currentplayer,
+  
+  
+}
+})();
+
+function makeplayer(name,mark){
+  return {
+   name,
+   mark,
+   
+  }
+ };
+
+
+const Game=(()=>{
+  
+  const start=()=>{
+      
+    mes.textContent=`Player ${GameDisplay.currentplayer.mark}'s turn!`;
+    
+    Gameboard.setborad();
+    Gameboard.display();
+    GameDisplay; 
+  }
+  
+  const changeplayer=(p)=>{
+    GameDisplay.currentplayer= p==GameDisplay.p1 ? GameDisplay.p2 : GameDisplay.p1;
+}
+
+  const play = (e) => {
+    
+    if (e.target.innerText != "") {
+      return;
+    }
+   
+    
+    
+    let id = parseInt(e.target.id);
+    let mark = (GameDisplay.currentplayer.mark);
+    e.target.innerText = mark;
+    
+    console.log(Gameboard.getborad());
+    Gameboard.adjustboard(id, mark);
+    console.log(id, mark); console.log(typeof(id), typeof(mark));
+    
+    changeplayer(GameDisplay.currentplayer);
+    mes.textContent =`Player ${GameDisplay.currentplayer.mark}'s turn!`;
+    console.log(Gameboard.getborad());
+    
+    checkwin(GameDisplay.currentplayer);
+    checkdraw();
+  };
+
+  let wincombos=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8]
+   ];
+  
+  
+  function checkwin(p) {
+   
+    for (let i=0;i<wincombos.length;i++) {
+      const [a, b, c] = wincombos[i];
+      console.log("index ",i);
+    
+      if (
+        Gameboard.getborad()[a] &&
+        Gameboard.getborad()[a] == Gameboard.getborad()[b] &&
+        Gameboard.getborad()[b] == Gameboard.getborad()[c]
+      ) 
+      {
+       
+
+        
+        
+        alert(`Player ${p.name} is the Winner`);
+        
+        reset();
+        break;
+      } 
+      
+      
+     }
+    }
+  function checkdraw(){
+    let j=0;
+    for (let index = 0; index < Gameboard.getborad().length; index++) {
+      const element = Gameboard.getborad()[index];
+
+       if(Gameboard.getborad()[index]!="")
+        { j++};
+  } 
+      if(j==9) 
+      {
+      console.log("index for tie",j);
+      
+     
+      alert(`TIEEEEEEEEEEEEEEEEE`);
+      
+      gamestate = false; 
+    reset();}
+  }
+
+  const reset=()=>{
+    count=0;  
+    mes.textContent = "";
+    Gameboard.setborad();
+    Gameboard.display();
     
    
-  };
-
-  return {
-    render,
-    getborad,
-    setborad
-  };
-})();
-
-const createplayer = (name, mark) => {
-  return {
-    name,
-    mark,
-    played: [],
-  };
-};
-
-const Game = (() => {
- 
-  let count =0;
-  let players = [];
-  const wincond=[
-   [0,1,2],
-   [3,4,5],
-   [6,7,8],
-   [0,3,6],
-   [1,4,7],
-   [2,5,8],
-   [2,4,6],
-   [0,4,8]
-  ];
-  const checkwin=((p)=>{
-     
-   wincond.some(combo=>{
-      if(combo.every(index=>p.played.includes(index) )){
-         document.getElementById("mesg").firstElementChild.textContent=`YOU win ${p.name}`;
-         players[0].played=[];
-         players[1].played=[];
-         count=0;
-         Gameboard.render();
-
-         
-      }
-     
-      
-      
-      
-   })
-   }
-  );
-
-  let turn = true;
-  
-
-  const start = () => {
-  
-    document.getElementById("mesg").firstElementChild.textContent= "";
-    players = [
-      createplayer(document.querySelector("#p1").value, "X"),
-      createplayer(document.querySelector("#p2").value, "O"),
-    ];
-    
-    count=0;
-    Gameboard.render();
-    
-  };
-  // This function handle click for each cell and store the postion of mark on the board
-  const hclick = (e) => {
-    console.log(count);
-     
-    let index = parseInt(e.target.id);
-   if (e.target.textContent=="") {
-    
-    if (turn) {
-      e.target.innerHTML = players[0].mark;
-      Gameboard.getborad()[index]=players[0].mark;
-      console.log(Gameboard.getborad());
-      e.target.disabled=true;
-      
-      let p1= players[0].played.push(index);
-      checkwin(players[0]);
-      ++count;
-      turn = false;
-    } 
-    else {
-      
-      e.target.innerHTML = players[1].mark;
-      Gameboard.getborad()[index]=players[1].mark;
-      console.log(Gameboard.getborad());
-      e.target.disabled=true;
-      ++count;
-      
-      let p1= players[1].played.push(index);
-      checkwin(players[1]);
-      
-      
-      turn = true;
-    }
-    if (count==9)
-
-    { 
-     document.getElementById("mesg").firstElementChild.textContent= "TIE";
-     count=0;
-     players[0].played=[];
-     players[1].played=[];
-     Gameboard.render();
-    }  
   }
-  };
- 
-  return {
-    start,
-    hclick,
-    start
-  };
+
+return {
+  start,
+  play,
+  reset,
+}
+
 })();
 
-//create board
-btn.addEventListener("click", () => {
-  Game.start();
-});
-btnrest.addEventListener("click", () => {
-   Game.start();
- });
+btn.addEventListener("click",Game.start);
